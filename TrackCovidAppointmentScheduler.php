21f91100656dc6696730e82ceed8d3fe90a70e04
Ihab Zeedia <ihab.zeedia@gmail.cm>
2020-07-29 10:23:49 -0700

    fix date for baseline.

--- TrackCovidAppointmentScheduler.php
+++ TrackCovidAppointmentScheduler.php
@@ -1573,9 +1573,8 @@ class TrackCovidAppointmentScheduler extends \ExternalModules\AbstractExternalMo
                 }
 
             } else {
-                $start = date('Y-m-d');
-                # change logic to get the next 14 days instead o just the end of this month.
-                $end = date('Y-m-d', strtotime('+365 days'));
+                $start = date('Y-m-d', strtotime('+7 days'));
+                $end = date('Y-m-d', strtotime('+30 days'));
             }
 
             return '<button data-baseline="' . $this->getBaseLineDate() . '"  data-month="' . $month . '"  data-year="' . $year . '" data-url="' . $url . '" data-record-id="' . $user['id'] . '" data-key="' . $eventId . '" data-offset="' . $offset . '" class="get-list btn btn-success">Schedule</button><br><small>(Schedule between ' . $start . ' and ' . $end . ')</small>';
