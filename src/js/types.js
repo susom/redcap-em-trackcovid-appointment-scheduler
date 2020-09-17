8729f9f55d774052584bba525690d57418b97de9
Ihab Zeedia <ihab.zeedia@gmail.cm>
2020-09-17 09:41:31 -0700

    fix location cookie error.

--- src/js/types.js
+++ src/js/types.js
@@ -477,17 +477,11 @@ jQuery(document).on('click', '.booked-slots', function (e) {
                                 $('#location-options option').each(function () {
 
                                     if (this.value === getCookie('preferred-location')) {
-                                        console.log(this.value)
-                                        console.log(getCookie('preferred-location'))
-                                        console.log(this.value == getCookie('preferred-location'))
-                                        console.log(this.value === getCookie('preferred-location'))
                                         exists = true;
                                         return false;
                                     }
                                 });
                                 if (exists === true) {
-                                    console.log('exists')
-                                    console.log(exists)
                                     $("#location-options").val(getCookie('preferred-location')).trigger('change');
                                 }
                             }
@@ -612,7 +606,18 @@ jQuery(document).on('click', '.manage-calendars', function (e) {
 
                                 // if preferred location is saved then select that
                                 if (getCookie('preferred-location') != null) {
-                                    $("#location-options").val(getCookie('preferred-location')).trigger('change');
+                                    // need to check the preferred location does exist in the list.
+                                    var exists = false;
+                                    $('#location-options option').each(function () {
+
+                                        if (this.value === getCookie('preferred-location')) {
+                                            exists = true;
+                                            return false;
+                                        }
+                                    });
+                                    if (exists === true) {
+                                        $("#location-options-manager").val(getCookie('preferred-location')).trigger('change');
+                                    }
                                 }
                             });
                         }
