746a5a365ff6d6a1fe4709df4394a1186ddee2ed
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-02-06 09:16:06 -0800

    display all errors.

--- src/booked_slots.php
+++ src/booked_slots.php
@@ -3,6 +3,9 @@
 
 namespace Stanford\TrackCovidSharedAppointmentScheduler;
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 /** @var \Stanford\TrackCovidSharedAppointmentScheduler\TrackCovidSharedAppointmentScheduler $module */
 
 try {
