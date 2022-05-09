cd715cd349d315006a4aada380443efb72f18583
ihabz <ihab.zeedia@stanford.edu>
2022-05-09 14:14:27 -0700

    fix compatibility issues.

--- TrackCovidSharedCalendarEmail.php
+++ TrackCovidSharedCalendarEmail.php
@@ -424,7 +424,7 @@ class TrackCovidSharedCalendarEmail extends Message
         }
     }
 
-    public function send()
+    public function send($removeDisplayName = false, $recipientIsSurveyParticipant = null, $enforceProtectedEmail = false, $emailCategory = null, $lang_id = null)
     {
         try {
             $email = new PHPMailer();
